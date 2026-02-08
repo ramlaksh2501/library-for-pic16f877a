035918fb7b5d6bbcba171e5810402956cf3e41c3
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-02-09 04:30:46 +0530

    updated gpio_DSP.h (added new functions and enums

--- GPIOS/gpio_DSP.h
+++ GPIOS/gpio_DSP.h
@@ -4,6 +4,29 @@
 #ifndef GPIO
 #define GPIO
 
+typedef enum {
+    output=0,
+    input
+}gpio_mode;
+
+
+typedef enum {
+   low=0,
+   high
+}pin_mode;
+
+
+typedef enum gpio{
+    zero=0,
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven
+}pin_rd;
+
 
 #define  INPUT  1
 #define  OUTPUT 0
@@ -15,16 +38,17 @@
 #define  LOW   0
 #define  SETHIGH(n,port) (port|=1<<(n))
 #define  SETLOW(n,port)  (port&=~(1<<(n)))
-#define  SETHIGH(n,port) (port=0xff)
-#define  SETLOW(n,port)  (port=0x00)
-void pin_config();
-void port_config(char a,);
-void pin_write();
-void port_write(char a,);
-void pin_read();
-void port_read(char a,);
-void led_blink();
-void led_chase();
+#define  SETPHIGH(n,port) (port=0xff)
+#define  SETPLOW(n,port)  (port=0x00)
+//input parameter for all the read,write and config functions : ('A'- 'E') ,(0-7) except port E for port E(0-2)
+void pin_config(char a,gpio_mode x);
+void port_config(char a,gpio_mode x);
+void pin_write(char a,pin_mode x);
+void port_write(char a,pin_mode x);
+void pin_read(char a,pin_rd);
+void port_read(char a);
+void led_blink();//only in port b (entire port will blink)
+void led_chase();//only in port b
 
 
 
