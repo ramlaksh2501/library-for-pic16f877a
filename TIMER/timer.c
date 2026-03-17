fda048c13f2b2de0fd9cb450886c26c3eaa7025b
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-03-17 18:59:35 +0530

    modified the delay function

--- TIMER/timer.c
+++ TIMER/timer.c
@@ -1,16 +1,9 @@
 #include "timer.h"
 
 void L_delay_ms(unsigned long t){
-   
-    // OPTION_REG&=~(1<<3);//set the prescaler to TMR0
-    // OPTION_REG&=~(1<<5);//set TMR0 to count per instruction cycle
-    // INTCON|=(1<<7);//enable global interrupt
-    // OPTION_REG|=(1<<2);//assigning the prescaler as 1:16
-    // INTCON&=~(1<<2);
     OPTION_REG=0x04;
     INTCON=0x00;
-    TMR0=0x65;//delay for 1 ms at 20 MHz 
-    
+    TMR0=0x65;
     unsigned long i=0;
     while(i!=t){
         if(INTCON & (1<<2)){
--- TIMER/timer.h
+++ TIMER/timer.h
@@ -1,9 +1,15 @@
 #ifndef TIMERS
 #define TIMERS
-
+#define TMR1_ON  TMR1CON_BITS->STATUS=1;
+#define TMR1_OFF TMR1CON_BITS->STATUS=0;
+#define TMR2_ON  TMR2CON_BITS->STATUS=1;
+#define TMR2_OFF TMR2CON_BITS->STATUS=0; 
 //blocking
 void L_delay_ms(unsigned long t);//all calculations are made in the 20MHz operation of the controller
 void L_delay_s(unsigned long t);
+void L_pwm(unsigned long long t);
+
+void __interrupt() void  myhandler();
 
 //non blocking 
 // a function to set time and a fucntions of the isr 
