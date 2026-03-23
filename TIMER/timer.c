57ecb15504d36f472bc0039015c94491c6be902f
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-03-23 07:40:52 +0530

    added and defined function for pwm

--- TIMER/timer.c
+++ TIMER/timer.c
@@ -1,6 +1,7 @@
 #include "timer.h"
 
 
+
 void L_delay_ms(unsigned long t){
     OPTION_REG=0x04;
     INTCON=0x00;
@@ -38,13 +39,31 @@ static inline void TMR1_OFF (){  TMR1CON_BITS->STATUS=0;}
 static inline void TMR2_ON  (){  TMR2CON_BITS->STATUS=1;}
 static inline void TMR2_OFF (){  TMR2CON_BITS->STATUS=0;}
 
-void init_pwm(){
-   TRISC&=~(1<<2);
+void init_pwm_default(){//the frequency is fixed in here 1.2khz ,the time period is aroud 800uS .
+// if you want to control the time period use the pwm config fuction
+//need to define the DUTY macro to use this fuciton 
+//DUTY must be below 1024 (0-1023)  duty cycle percentage = (DUTY/1024)*100
+//#define DUTY (value from 0 to 1023) 
+//this fucntion used ccp1 so the output will be in the pin C2
+    PR2=0xff;
     CCP1CON|=0x0F;
     TMR2_OFF();
+    TRISC&=~(1<<2);
+    #ifdef DUTY
+    if(DUTY >=0 && DUTY<=1023){
+        CCPR1L=DUTY>>2;
+        CCP1CON|=(unsigned char)(DUTY&0x03)<<4;
+    }
+    #endif
+   
     TMR2CON_BITS->PRESCALE=0x3;
-    
-    
+}
+
+void pwm_config(){
+    //give the prescale 
+    //value for the pr2
+    //DUTY macro must be defined 
+    //#define DUTY (value from 0 to 1023) 
 
 }
 
@@ -52,9 +71,7 @@ void init_pwm(){
 
 void L_pwm(){
 
-PR2=0xFF;
-CCP1CON|=(0x03)<<4;
-CCPR1L=0x7F;
+
 
 }
 
--- TIMER/timer.h
+++ TIMER/timer.h
@@ -1,16 +1,19 @@
 #ifndef TIMERS
 #define TIMERS
- #include"/home/ramlaksh/learnings/pic-library/GPIOS/registers.h"
+
 //blocking
 void L_delay_ms(unsigned long t);//all calculations are made in the 20MHz operation of the controller
 void L_delay_s(unsigned long t);
 void L_pwm();
-void init_pwm();
+void init_pwm_default();
+void pwm_config();
 //void __interrupt() void  myhandler();
 static inline void TMR1_ON  ();
 static inline void TMR1_OFF ();
 static inline void TMR2_ON  ();
 static inline void TMR2_OFF ();
+static inline void PWM_START();
+static inline void PWM_STOP ();
 //non blocking 
 // a function to set time and a fucntions of the isr 
 // for example timersISR(time_delay,funciton to run inside the isr after that time)
