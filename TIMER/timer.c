a4ce1f3c835ec0d11794d17333ad83d97a6c2d66
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-03-24 15:45:42 +0530

    finally completed the pwm funcitons

--- TIMER/timer.c
+++ TIMER/timer.c
@@ -39,40 +39,50 @@ static inline void TMR1_OFF (){  TMR1CON_BITS->STATUS=0;}
 static inline void TMR2_ON  (){  TMR2CON_BITS->STATUS=1;}
 static inline void TMR2_OFF (){  TMR2CON_BITS->STATUS=0;}
 
-void init_pwm_default(){//the frequency is fixed in here 1.2khz ,the time period is aroud 800uS .
+void init_pwm(uint16_t duty){//the frequency is fixed in here 1.2khz ,the time period is aroud 800uS .
 // if you want to control the time period use the pwm config fuction
-//need to define the DUTY macro to use this fuciton 
+//the parameter duty denotes the duty cycle of the pwm 
 //DUTY must be below 1024 (0-1023)  duty cycle percentage = (DUTY/1024)*100
-//#define DUTY (value from 0 to 1023) 
 //this fucntion used ccp1 so the output will be in the pin C2
     PR2=0xff;
     CCP1CON|=0x0F;
     TMR2_OFF();
+    TMR2CON_BITS->PRESCALE=0x3;  
     TRISC&=~(1<<2);
-    #ifdef DUTY
-    if(DUTY >=0 && DUTY<=1023){
-        CCPR1L=DUTY>>2;
-        CCP1CON|=(unsigned char)(DUTY&0x03)<<4;
+    if(duty<=1023){
+        CCPR1L=(unsigned char)duty>>2;
+        CCP1CON|=(unsigned char)(duty&0x03)<<4;
     }
-    #endif
+    TMR2_ON();
+    
    
-    TMR2CON_BITS->PRESCALE=0x3;
+   
 }
 
-void pwm_config(){
-    //give the prescale 
+void pwm_config(uint8_t pr2,uint8_t prescale ,uint16_t duty){
+    //parameters: 
     //value for the pr2
-    //DUTY macro must be defined 
+    //prescale value of tmr2 (must be one of these : TMR_PRESCALE_1 or TMR_PRESCALE_4 or TMR_PRESCALE_16 macros)
+    //duty  in the range 0-1023
+    //duty cycle percentage =(duty/1023)*100
+    //refer the formula for time period in datasheet 
     //#define DUTY (value from 0 to 1023) 
+    TMR2_OFF();
+    TRISC&=~(1<<2);
+    if( duty<=1023){
+        CCPR1L=(unsigned char)duty>>2;
+        CCP1CON|=(unsigned char)(duty&0x03)<<4;
+    }
+    TMR2CON_BITS->prescale=prescale;
+    PR2=pr2;
+    TMR2_ON();
 
+    
 }
 
 
-
-void L_pwm(){
-
-
-
-}
-
+static inline void PWM_START(){
+    TMR2_ON();}
+static inline void PWM_STOP (){
+    TMR2_OFF();}
 
--- TIMER/timer.h
+++ TIMER/timer.h
@@ -1,12 +1,15 @@
 #ifndef TIMERS
 #define TIMERS
+#define TMR_PRESCALE_1  0x00
+#define TMR_PRESCALE_4  0x01
+#define TMR_PRESCALE_16 0x03
 
 //blocking
 void L_delay_ms(unsigned long t);//all calculations are made in the 20MHz operation of the controller
 void L_delay_s(unsigned long t);
 void L_pwm();
-void init_pwm_default();
-void pwm_config();
+void init_pwm(uint16_t duty);
+void pwm_config(uint8_t pr2,uint8_t prescale ,uint16_t duty);
 //void __interrupt() void  myhandler();
 static inline void TMR1_ON  ();
 static inline void TMR1_OFF ();
