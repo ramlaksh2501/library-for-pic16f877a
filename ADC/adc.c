f8f0320452a77baba9efdf49b4fb9c089d30c313
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-03-25 15:25:01 +0530

    added analog read function

--- ADC/adc.c
+++ ADC/adc.c
@@ -1,1 +1,23 @@
+#ifndef ADCIN
+#define ADCIN
+
+
 #include "adc.h"
+
+
+uint16_t analog_read(ADC_pin x){
+
+    ADCON1|=1<<6;
+    ADCON0|=(x<<3);
+    ADCON1|=(1<<7);
+    ADCON0|=1<<2;
+    while(ADCON0 & 1<<2);
+    uint16_t res=0;
+    res=ADRESH<<7 | ADRESL;
+    return res;
+}
+
+
+
+
+#endif
--- ADC/adc.h
+++ ADC/adc.h
@@ -12,10 +12,10 @@ typedef enum {
 }ADC_pin;
 void adc_on();
 void adc_off();
-unsigned short adc();//for pin A0
-unsigned short analog_read(ADC_pin x);
-unsigned short sleep_adc();//for pin A0
-unsigned short analog_read(ADC_pin x){
-    
-}
+
+uint16_t analog_read(ADC_pin x); // the return value will be in the range 0-1023 
+// analog value is (return value/1024)*(ref voltage ) V
+//by default the ref is 5 V (for more info refer datasheet)
+
+
 #endif
