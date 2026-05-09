2279b9e31a789657e71fae647175d10168cd8875
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-05-09 23:51:21 +0530

    added comments

--- ADC/adc.c
+++ ADC/adc.c
@@ -7,13 +7,14 @@
 
 uint16_t analog_read(ADC_pin x){
 
-    ADCON1|=1<<6;
-    ADCON0|=(x<<3);
-    ADCON1|=(1<<7);
-    ADCON0|=1<<2;
-    while(ADCON0 & 1<<2);
+    ADCON1|=1<<6; //initializing analog pins
+    ADCON0|=(x<<3);//selecting the input pin
+    ADCON1|=(1<<7);//making the result left justified
+    ADCON0|=1;//turning on the adc
+    ADCON0|=1<<2;//starting the conversion
+    while(ADCON0 & 1<<2);//checking if the conversion completed
     uint16_t res=0;
-    res=(uint16_t)ADRESH<<7 | ADRESL;
+    res=(uint16_t)ADRESH<<7 | ADRESL;//storing the result
     return res;
 }
 
