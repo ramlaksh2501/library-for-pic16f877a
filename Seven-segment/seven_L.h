8a823356d5e4c449d8940fba431305408675c3bd
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-05-27 20:41:35 +0530

    fixed type mismatch and spelling mistakes

--- Seven-segment/seven_L.h
+++ Seven-segment/seven_L.h
@@ -2,7 +2,7 @@
 #define SEVEN
 
 void config_sevenseg();//any port can be used except port a and port e **for single digit 
-unsigned sevenseg_L(unsigned char x);//need to assign the return value to the desired port ***input range : 0-9***
+unsigned char sevenseg_L(unsigned char x);//need to assign the return value to the desired port ***input range : 0-9***
 unsigned char sevenseg_AL_(unsigned char x);// for common anode
 void config_sevenmux();//use port b for sevensegment connect from A of sevensegment  from lsb of portb and the 
 //common cathode pins of each sevensegment from port C lsb ***refer example code for more details***
