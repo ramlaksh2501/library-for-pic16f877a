0615e87037278d2994221c4ec7c3428817362037
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-06-01 23:17:35 +0530

    removed the enum due to errors

--- GPIOS/gpio_DSP.h
+++ GPIOS/gpio_DSP.h
@@ -6,16 +6,6 @@
 #ifndef GPIO
 #define GPIO
 
-typedef enum  {
-    output=0,
-    input
-}gpio_mode;
-
-
-typedef enum {
-   low=0,
-   high
-}pin_mode;
 
 
 typedef enum gpio{
@@ -33,11 +23,11 @@ typedef enum gpio{
 
 
 //input parameter for all the read,write and config functions : ('A'- 'E') ,(0-7) except port E for port E(0-2)
-void pin_config(char a,gpio_mode x,pin_rd p);//do not use port A
-void port_config(char a,gpio_mode x);//do not use port A
-void pin_write(char a,pin_mode x,pin_rd p);
+void pin_config(char a,unsigned char x,unsigned char  p);//do not use port A
+void port_config(char a,unsigned char  x);//do not use port A
+void pin_write(char a,unsigned char  x,unsigned char p);
 void port_write(char a,char x);
-char pin_read(char a,pin_rd p);//returns 0 or one
+char pin_read(char a,unsigned char  p);//returns 0 or one
 char port_read(char a);//returns the entire register of the port
 void led_blink();//only in port b (entire port will blink)
 void led_chase();//only in port b
