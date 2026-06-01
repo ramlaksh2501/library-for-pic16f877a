b2bee8f8c5bf0cc30d9a4c001d6d2ca289676720
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-06-01 23:16:38 +0530

    made changes and tested sucessfully (write)

--- I2C/i2c.c
+++ I2C/i2c.c
@@ -7,22 +7,42 @@ void i2c_begin(){
 	SSPCON|=(1<<3);
 }
 
-void i2c_write(unsigned char address,char * message){
-	SSPCON|=(1<<5);
-	SSPCON2|=1;
+void i2c_write(unsigned char address, char * message){
+ //     SSPCON|=(1<<5);
+ //	SSPCON2|=1;
+ //	while(!(PIR1&(1<<3)));
+ //	PIR1&=~(1<<3);
+ //	SSPBUF=address;
+ //	while(!(PIR1&(1<<3)));
+ //	PIR1&=~(1<<3);
+ //SEND : if(!*message) goto END;
+ //	SSPBUF=*message;
+ //	while(!(PIR1&(1<<3)));
+ //	PIR1&=~(1<<3);
+ //	message+=1;
+ //	goto SEND;
+ //END :	SSPCON2|=1<<2;
+ //	while(!(PIR1&(1<<3)));
+ //	PIR1&=~(1<<3);
+ if(*message==0) return ;
+       SSPCON|=(1<<5);
+ 	SSPCON2|=1;
+ 	while(!(PIR1&(1<<3)));
+ 	PIR1&=~(1<<3);
+ 	SSPBUF=address;
+		while(!(PIR1&(1<<3)));
+ 	PIR1&=~(1<<3);
+while(*message){
+
+ 	SSPBUF=*message;
 	while(!(PIR1&(1<<3)));
-	PIR1&=~(1<<3);
-	SSPBUF=address;
-	while(!(PIR1&(1<<3)));
-	PIR1&=~(1<<3);
-SEND : if(!*message) goto END;
-	SSPBUF=*message;
-	while(!(PIR1&(1<<3)));
-	PIR1&=~(1<<3);
-	message+=1;
-	goto SEND;
-END :	SSPCON2|=1<<2;
-	while(!(PIR1&(1<<3)));
-	PIR1&=~(1<<3);
+ 	PIR1&=~(1<<3);
+	message+=1;	
+}
+	SSPCON2|=(1<<2);
+ 	while(!(PIR1&(1<<3)));
+ 	PIR1&=~(1<<3);
+
+	
 }
 
