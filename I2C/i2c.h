884ac0e391d2a6f7a41ab0c889ef54d7d8578a3e
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-06-02 19:44:03 +0530

    changed the arguments of receive function and also made the communication speed 400khz from 1Mhz

--- I2C/i2c.h
+++ I2C/i2c.h
@@ -5,9 +5,8 @@
 
 
 void i2c_begin();
-void i2c_write(unsigned char address, char * message);
-unsigned char * i2c_read(unsigned char address);
-
+void i2c_write(unsigned char address, char * message);//7 bit address of the i2c device and the pointer to the message to be sent (must be null terminated)
+unsigned char * i2c_read(unsigned char address,char *message,unsigned char size);//a pointer to the recived msg is returned 
 
 
 
